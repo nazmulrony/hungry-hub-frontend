2761341dea0661fe6d01751b56ce7d28904fca37
nazmulrony <nhrony65@gmail.com>
2024-03-23 03:13:27 +0600

    after call back working

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,40 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+// import { handleAuth } from "@auth0/nextjs-auth0";
 
-export const GET = handleAuth();
+// export const GET = handleAuth();
+
+import {
+    handleAuth,
+    handleCallback,
+    AppRouteHandlerFnContext,
+    Session,
+    getSession,
+    AfterCallbackAppRoute,
+} from "@auth0/nextjs-auth0";
+import { NextRequest, NextResponse } from "next/server";
+
+const afterCallback: AfterCallbackAppRoute = (
+    req: NextRequest,
+    session: Session
+) => {
+    console.log("User Authe");
+    if (session.user) {
+        return session;
+    }
+};
+
+export const GET = handleAuth({
+    async callback(req: NextRequest, ctx: AppRouteHandlerFnContext) {
+        const res = (await handleCallback(req, ctx, {
+            afterCallback,
+        })) as NextResponse;
+        const session = await getSession(req, res);
+        if (session) {
+            return NextResponse.redirect(`${process.env.AUTH0_BASE_URL}`, res);
+        } else {
+            return NextResponse.redirect(`${process.env.AUTH0_BASE_URL}`, res);
+        }
+    },
+    onError(req: Request, error: Error) {
+        console.error(error);
+    },
+});
